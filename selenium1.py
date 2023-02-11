f1fdb9e412707429c80858053779119fe8294ec4
danielkorkus <daniel.korkus94@gamil.com>
2023-02-11 14:28:31 +0100

    5th commit

---
+++ selenium1.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+import time
+
+driver = webdriver.Firefox(executable_path=r".\geckodriver.exe")
+driver.get('https://www.google.com/')
+print("Nazwa strony", driver.title)
+time.sleep(3)
+driver.quit()
+
+button1_srch = driver.find_element('id', 'L2AGLb')  # nie uzywaj find by ID bo jest to przestarzale
+print(button1_srch)
+button1_srch.click()
+srch_bar = driver.find_element('name', 'q')
+srch_bar.send_keys("Hello World!")
+srch_btn2 = driver.find_element('name', 'btnK')
+srch_btn2.submit()
