d060da7deaf144bd7b665657deac2d47b542b20a
danielkorkus <daniel.korkus94@gamil.com>
2023-02-11 12:35:17 +0100

    4th commit

--- functions.py
+++ functions.py
@@ -1,9 +1,22 @@
-def multiply(x, y):
-    if isinstance(x, (int, float)) and isinstance(y, (int, float)):
-        return round(x * y, 5)
+# def multiply(x, y):
+#     if isinstance(x, (int, float)) and isinstance(y, (int, float)):
+#         return round(x * y, 5)
+#     else:
+#         return x * y
+#
+#
+# def no_of_letter(text):
+#     return len(str(text))
+
+
+def fissbuzz(i):
+    if isinstance(i, (str, float)): return int(i)
+    if i < 0: return None
+    if i % 3 == 0 and i % 5 == 0:
+        return "fissbuzz"
+    elif i % 5 == 0:
+        return "buzz"
+    elif i % 3 == 0:
+        return "fiss"
     else:
-        return x * y
-
-
-def no_of_letter(text):
-    return len(str(text))
+        return i
--- tdd_test.py
+++ tdd_test.py
@@ -1,18 +1,29 @@
 from functions import *
 
 
-def test_multiply():
-    assert multiply(5, 10) == 50
-    assert multiply(100, 1.1) == 110
-    assert multiply(1.5, 1.5) == 2.25
-    assert multiply(0.0000001, 100) == 0.00001
-    assert multiply('mama', 3) == 'mamamamamama'
-    assert multiply(None,5) == None
+# def test_multiply():
+#     assert multiply(5, 10) == 50
+#     assert multiply(100, 1.1) == 110
+#     assert multiply(1.5, 1.5) == 2.25
+#     assert multiply(0.0000001, 100) == 0.00001
+#     assert multiply('mama', 3) == 'mamamamamama'
+#     assert multiply(None, 5) == None
+#
+#
+# def test_no_of_letter():
+#     assert no_of_letter("Hello World!")
+#     assert no_of_letter(f"Dog {115}")
+#     assert no_of_letter("Cat" + "Dog")
+#     assert no_of_letter("Dog {} Cat {}".format(15, 20))
+#     assert no_of_letter(115)
 
 
 def test_no_of_letter():
-    assert no_of_letter("Hello World!")
-    assert no_of_letter(f"Dog {115}")
-    assert no_of_letter("Cat" + "Dog")
-    assert no_of_letter("Dog {} Cat {}".format(15, 20))
-    assert no_of_letter(115)
+    assert fissbuzz(1.3) == 1
+    assert fissbuzz(1.9) == 1
+    assert fissbuzz(4) == 4
+    assert fissbuzz("1") == 1
+    assert fissbuzz(3) == 'fiss'
+    assert fissbuzz(15) == 'fissbuzz'
+    assert fissbuzz(5) == 'buzz'
+    assert fissbuzz(-5) == None
