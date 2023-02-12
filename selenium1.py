4e890fd6e7943577ba5eff8c107b4ba63d4c3d85
danielkorkus <daniel.korkus94@gamil.com>
2023-02-12 09:06:28 +0100

    7th commit

--- selenium1.py
+++ selenium1.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from time import sleep
 
-driver = webdriver.Edge(executable_path=r'C:\Users\danie\OneDrive\Desktop\#python\msedgedriver.exe')
+driver = webdriver.Edge(executable_path=r'.\msedgedriver.exe')
 driver.get('https://www.google.com/')
 sleep(3)
 driver.quit()
